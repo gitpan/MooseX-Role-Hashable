012f84933459524c0ac25993b27faca8676f3a7e
Aaron Cohen <morninded@cpan.org>
2014-12-30 22:44:23 +0000

    Import of MORNINDED/MooseX-Role-Hashable-1.04.tar.gz from CPAN.
    gitpan-cpan-distribution: MooseX-Role-Hashable
    gitpan-cpan-version:      1.04
    gitpan-cpan-path:         MORNINDED/MooseX-Role-Hashable-1.04.tar.gz
    gitpan-cpan-author:       MORNINDED
    gitpan-cpan-maturity:     released

--- lib/MooseX/Role/Hashable.pm
+++ lib/MooseX/Role/Hashable.pm
@@ -10,22 +10,23 @@ use strict;
 use warnings;
 
 use Moose::Role;
-use Set::Functional qw{setify_by};
+use Set::Functional qw{difference_by setify_by};
 use namespace::autoclean;
 
 =head1 VERSION
 
-Version 1.03
+Version 1.04
 
 =cut
 
-our $VERSION = '1.03';
+our $VERSION = '1.04';
 
 =head1 SYNOPSIS
 
 This module adds a single method to an object to convert it into a simple hash.
-In some ways, this can be seen as the inverse function to I<new>, provided
-nothing too crazy is going on during initialization.
+This is meant to act as the inverse function of I<new>, provided nothing too
+crazy is going on during initialization.  If the class is made immutable, the
+optimizer will precalculate the extracted attributes for a signifcant speed up.
 
 Example usage:
 
@@ -36,14 +37,15 @@ Example usage:
 	has field1 => (is => 'rw');
 	has field2 => (is => 'ro');
 	has field3 => (is => 'bare');
+	has _field4 => (is => 'rw', init_arg => 'field4');
 
 	__PACKAGE__->meta->make_immutable;
 
 	package main;
 
-	my $foo = Foo->new(field1 => 'val1', field2 => 'val2', field3 => 'val3');
+	my $foo = Foo->new(field1 => 'val1', field2 => 'val2', field3 => 'val3', field4 => 'val4');
 	$foo->as_hash;
-	# => {field1 => 'val1', field2 => 'val2', field3 => 'val3'}
+	# => {field1 => 'val1', field2 => 'val2', field3 => 'val3', field4 => 'val4'}
 
 =cut
 
@@ -78,23 +80,52 @@ do {
 =head2 as_hash
 
 Transform the object into a hash of attribute-value pairs.  All attributes,
-including those without a reader, are extracted.  If a value is a reference,
-as_hash will perform a shallow copy.
+including those without a reader, are extracted.  Attributes whose initial
+arguments differ from their name will appear using the initialization argument.
+Attributes which can' be initialized will be ignored.  Reference values will
+perform a shallow copy.
 
 =cut
 
-my %CLASS_TO_UNINITIALIZED_ATTRIBUTES;
+my %CLASS_TO_ATTRIBUTES;
+my $extract_attributes_ref = sub {
+	return
+		#We only want one copy of each attribute
+		setify_by { $_->name }
+		#Manually taverse all attributes, get_all_attributes doesn't update
+		#with superclass changes afte subclass immutability
+		map { my $meta = $_->meta; map { $meta->get_attribute($_) } $meta->get_attribute_list }
+		#Make sure attribute overrides take precedence
+		reverse $_[0]->meta->linearized_isa;
+};
+my $extract_ignored_ref = sub { grep { ! $_->has_init_arg } @_ };
+my $extract_translated_ref = sub { map { ($_->name => $_->init_arg) } grep { $_->has_init_arg && $_->init_arg ne  $_->name } @_ };
+my $extract_uninitialized_ref = sub { grep { ! ($_->is_required || ! $_->is_lazy && ($_->has_builder || $_->has_default)) } @_ };
+my $prepare_attributes_ref = sub {
+	my @ignored = $extract_ignored_ref->(@_);
+	my %translated = $extract_translated_ref->(@_);
+	my @uninitialized = $extract_uninitialized_ref->(@_);
+	return (
+		[map { $_->name } @ignored],
+		\%translated,
+		[difference_by { $_->name } \@uninitialized, \@ignored],
+	)
+};
 
 sub as_hash {
 	my $self = shift;
 
-	my $uninitialized_attributes = exists $CLASS_TO_UNINITIALIZED_ATTRIBUTES{ref $self}
-		? $CLASS_TO_UNINITIALIZED_ATTRIBUTES{ref $self}
-		: [$self->meta->get_all_attributes];
+	my $cached_attributes = $CLASS_TO_ATTRIBUTES{ref $self};
+	my ($ignored_attributes, $translated_attributes, $uninitialized_attributes) = $cached_attributes
+		? @{$cached_attributes}{qw{ignored translated uninitialized}}
+		: $prepare_attributes_ref->($extract_attributes_ref->($self))
+		;
 
 	my %copy = %$self;
 	$copy{$_->name} = $_->get_value($self)
 		for grep { ! exists $copy{$_->name} } @$uninitialized_attributes;
+	@copy{values %$translated_attributes} = delete @copy{keys %$translated_attributes};
+	delete @copy{@$ignored_attributes};
 
 	return \%copy;
 }
@@ -102,16 +133,9 @@ sub as_hash {
 sub optimize_as_hash {
 	my $class = shift;
 
-	@{$CLASS_TO_UNINITIALIZED_ATTRIBUTES{$class}} =
-		#Find all fields that aren't guaranteed to exist
-		grep { ! ($_->is_required || ! $_->is_lazy && ($_->has_builder || $_->has_default)) }
-		#We only want one copy of each attribute
-		setify_by { $_->name }
-		#Manually taverse all attributes, get_all_attributes doesn't update
-		#with superclass changes afte subclass immutability
-		map { my $meta = $_->meta; map { $meta->get_attribute($_) } $meta->get_attribute_list }
-		#Make sure attribute overrides take precedence
-		reverse $class->meta->linearized_isa;
+	#Precalculate the attributes
+	@{$CLASS_TO_ATTRIBUTES{$class}}{qw{ ignored translated uninitialized }} =
+		$prepare_attributes_ref->($extract_attributes_ref->($class));
 
 	$_->optimize_as_hash for $class->meta->direct_subclasses;
 
@@ -121,7 +145,8 @@ sub optimize_as_hash {
 sub deoptimize_as_hash {
 	my $class = shift;
 
-	delete $CLASS_TO_UNINITIALIZED_ATTRIBUTES{$class};
+	delete $CLASS_TO_ATTRIBUTES{$class};
+
 	$_->deoptimize_as_hash for $class->meta->direct_subclasses;
 
 	return;
